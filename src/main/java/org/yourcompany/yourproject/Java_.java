bfb8d0bbda026287f49baf689b8ed84cfe4dbd8c
Xx_MarquitosGamer777_xX <Marcojojas777@gmail.com>
2025-09-19 11:58:21 +0200

    Codigo arreglado

--- src/main/java/org/yourcompany/yourproject/Java_.java
+++ src/main/java/org/yourcompany/yourproject/Java_.java
@@ -8,5 +8,7 @@ public class Java_ {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Soy dios y una patata");
+        System.out.println("me gusta vegetta777");
     }
 }
